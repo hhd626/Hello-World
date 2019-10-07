5f1dcdf57d61f31b15ab72b96d51fe7d541dd7bd
hhd626 <56241054+hhd626@users.noreply.github.com>
2019-10-07 12:00:51 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+     int x=0;
+        System.out.print("x is ");
+        System.out.println( x);
+     x=15;
+        System.out.print("x is now ");
+        System.out.println(x);
+     int y;
+     y = x+1;
+        System.out.print("x and y is ");
+        System.out.print(x);
+        System.out.print( " and " );
+        System.out.print(y);
+    }
+}
+
+
