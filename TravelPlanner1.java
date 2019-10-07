dc326783ed8a8d0421a897584ca1feb06ff80258
hhd626 <56241054+hhd626@users.noreply.github.com>
2019-10-08 05:10:14 +0800

    Update TravelPlanner1.java

--- TravelPlanner1.java
+++ TravelPlanner1.java
@@ -45,8 +45,7 @@ public class TravelPlanner1 {
         int time = input.nextInt();
         if (time >0) {
             System.out.println("That means that when it is midnight at home it will be " + time + ":00 in your travel destination " + "and when it is noon at home it will be " + (12+time)%24+ ":00");
-        }
-            else {   
+        } else {   
             System.out.println("That means that when it is midnight at home it will be " + (24+time) + ":00 in your travel destination and when it is noon at home it will be " + (12+time)+ ":00" );
         }
         System.out.println("************\n");
